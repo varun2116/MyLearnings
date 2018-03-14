3842edaf61abb6e5bf04321e319a9b75f6b42b85
Varun Gavini <gvarun@sapient.com>
2018-03-14 14:02:30 +0530

    Styled Components

--- React/03_StyledComponents/02layout/src/Layout.js
+++ React/03_StyledComponents/02layout/src/Layout.js
@@ -2,18 +2,14 @@ import React from "react";
 import logo from './logo.svg';
 import { App, AppHeader, AppIntro, Title, AppLogo} from './styles';
 
-const pStyles = {backgroundColor: "red"};
-
-export default function Home(){
+export default function Layout({title, renderContent}){
     return(
         <App>
             <AppHeader>
               <AppLogo src={logo} alt="logo" />
-              <Title>Welcome to React</Title>
+              <Title>{title}</Title>
             </AppHeader>
-            <AppIntro style={pStyles}>
-              To get started, edit <code>src/App.js</code> and save to reload.
-            </AppIntro>
+            {renderContent()}
         </App>
     );
 }
